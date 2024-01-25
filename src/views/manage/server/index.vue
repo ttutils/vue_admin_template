2c11f0982d6e14cf35be27a0ad1740b78f86289d
buyfakett <buyfakett@vip.qq.com>
2024-01-25 19:22:35 +0800

    中文化

--- src/views/manage/server/index.vue
+++ src/views/manage/server/index.vue
@@ -4,7 +4,7 @@
     <el-table
       v-loading="listLoading"
       :data="list"
-      element-loading-text="Loading"
+      element-loading-text="拼命加载中"
       border
       fit
       highlight-current-row
