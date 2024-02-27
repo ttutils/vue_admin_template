f70245e39e4e7df9c238356dc92b94a1ca7e3682
buyfakett <buyfakett@vip.qq.com>
2024-02-27 20:54:37 +0800

    :bug: fix: 修复首次登录调用接口不携带token

--- src/utils/request.js
+++ src/utils/request.js
@@ -13,14 +13,7 @@ const service = axios.create({
 // request interceptor
 service.interceptors.request.use(
   config => {
-    // do something before request is sent
-
-    if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      config.headers['AUTHORIZATION'] = getToken()
-    }
+    config.headers['AUTHORIZATION'] = getToken()
     return config
   },
   error => {
